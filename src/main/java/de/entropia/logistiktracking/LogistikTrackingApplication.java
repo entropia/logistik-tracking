8a789da043d4f62bb83c21775276b3f114617f4a
0x3C50 <99053360+0x3C50@users.noreply.github.com>
2025-02-22 20:35:06 +0100

    Initial commit

---
+++ src/main/java/de/entropia/logistiktracking/LogistikTrackingApplication.java
@@ -0,0 +1,13 @@
+package de.entropia.logistiktracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogistikTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogistikTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/entropia/logistiktracking/LogistikTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package de.entropia.logistiktracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogistikTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
