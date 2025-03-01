1e4bb62b64cc55bcec8611182a85e8ba7f173433
0x3C50 <99053360+0x3C50@users.noreply.github.com>
2025-03-01 20:27:21 +0100

    things idk

--- src/main/java/de/entropia/logistiktracking/jpa/repo/UserRepo.java
+++ src/main/java/de/entropia/logistiktracking/jpa/repo/UserRepo.java
@@ -1,9 +1,9 @@
 package de.entropia.logistiktracking.jpa.repo;
 
-import de.entropia.logistiktracking.jpa.User;
+import de.entropia.logistiktracking.jpa.DbUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepo extends JpaRepository<User, String> {
+public interface UserRepo extends JpaRepository<DbUser, String> {
 }
