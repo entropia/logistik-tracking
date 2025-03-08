f0a3ac495d006dce539fcca806b9d05a67c138c0
Alexandra Baier <alx.baier@gmail.com>
2025-03-08 17:16:51 +0100

    Added endpoints for retrieving all and specific euro crates.

---
+++ src/main/java/de/entropia/logistiktracking/domain/use_case/FindEuroCrateError.java
@@ -0,0 +1,6 @@
+package de.entropia.logistiktracking.domain.use_case;
+
+public enum FindEuroCrateError {
+    BadArguments,
+    CrateNotFound,
+}
---
+++ src/main/java/de/entropia/logistiktracking/domain/use_case/FindEuroCrateUseCase.java
@@ -0,0 +1,59 @@
+package de.entropia.logistiktracking.domain.use_case;
+
+
+import de.entropia.logistiktracking.domain.converter.EuroCrateConverter;
+import de.entropia.logistiktracking.domain.converter.OperationCenterConverter;
+import de.entropia.logistiktracking.domain.euro_crate.EuroCrate;
+import de.entropia.logistiktracking.domain.operation_center.OperationCenter;
+import de.entropia.logistiktracking.domain.repository.EuroCrateRepository;
+import de.entropia.logistiktracking.openapi.model.EuroCrateDto;
+import de.entropia.logistiktracking.openapi.model.OperationCenterDto;
+import de.entropia.logistiktracking.utility.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class FindEuroCrateUseCase {
+    private final EuroCrateRepository euroCrateRepository;
+    private final EuroCrateConverter euroCrateConverter;
+    private final OperationCenterConverter operationCenterConverter;
+
+    @Autowired
+    public FindEuroCrateUseCase(EuroCrateRepository euroCrateRepository, EuroCrateConverter euroCrateConverter, OperationCenterConverter operationCenterConverter) {
+        this.euroCrateRepository = euroCrateRepository;
+        this.euroCrateConverter = euroCrateConverter;
+        this.operationCenterConverter = operationCenterConverter;
+    }
+
+    public List<EuroCrateDto> findAllEuroCrates() {
+        return euroCrateRepository
+                .findAllEuroCrates()
+                .stream()
+                .map(euroCrateConverter::toDto)
+                .toList();
+    }
+
+    public Result<EuroCrateDto, FindEuroCrateError> findEuroCrate(OperationCenterDto operationCenterDto, String euroCrateName) {
+        if (operationCenterDto == null || euroCrateName == null || euroCrateName.isBlank()) {
+            return new Result.Error<>(FindEuroCrateError.BadArguments);
+        }
+
+        OperationCenter operationCenter;
+        try {
+            operationCenter = operationCenterConverter.from(operationCenterDto);
+        } catch (IllegalArgumentException e) {
+            return new Result.Error<>(FindEuroCrateError.BadArguments);
+        }
+
+        Optional<EuroCrate> euroCrate = euroCrateRepository.findEuroCrate(operationCenter, euroCrateName);
+
+        if (euroCrate.isEmpty()) {
+            return new Result.Error<>(FindEuroCrateError.CrateNotFound);
+        }
+
+        return new Result.Ok<>(euroCrateConverter.toDto(euroCrate.get()));
+    }
+}
