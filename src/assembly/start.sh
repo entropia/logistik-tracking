e7ef51bf0ab387c16d7bff9ba8e0a9895cfb345c
0x3C50 <99053360+0x3C50@users.noreply.github.com>
2025-05-09 01:17:37 +0200

    frontend vom bin aus serven

--- src/assembly/start.sh
+++ src/assembly/start.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ZUM LETZTEN MAL DIGGA CHECK OB DU DIE RICHTIGE DATEI BEARBEITEST!!! BIST DU IN DER GEBAUTEN ODER IM TEMPLATE??
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-JAR_PATH="$(dirname "$0")/@artifactId@-@version@.jar"
+
+BIN_DIR=$(dirname "$0")
+JAR_PATH=$(realpath "$BIN_DIR/@artifactId@-@version@.jar")
 
 # use JAVA_HOME if set, otherwise use java from PATH
 if [ -n "$JAVA_HOME" ]; then
@@ -9,7 +18,11 @@ else
   JAVA_CMD="java"
 fi
 
+pushd "$BIN_DIR/.." || exit 9
+
 # lets go
 # shellcheck disable=SC2086
 #           V fuck you ron pressler
 "$JAVA_CMD" --enable-native-access=ALL-UNNAMED $JAVA_OPTS -jar "$JAR_PATH" "$@"
+
+popd || exit 9
