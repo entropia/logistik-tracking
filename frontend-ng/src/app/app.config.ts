8375e4b99b704b7600a6d48d4d493a9758810590
0x3C50 <99053360+0x3C50@users.noreply.github.com>
2025-05-14 00:10:29 +0200

    add frontend environment depending on configuration

--- frontend-ng/src/app/app.config.ts
+++ frontend-ng/src/app/app.config.ts
@@ -6,14 +6,13 @@ import {ApiModule} from './api/api.module';
 import {HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors} from '@angular/common/http';
 import {HashLocationStrategy, LocationStrategy} from '@angular/common';
 import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+import {environment} from './environments/environment';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideZoneChangeDetection({eventCoalescing: true}),
 		provideRouter(routes, withComponentInputBinding()),
-		// FIXME das sollte eigentlich besser aussehen. funktioniert aber um den api host auf die jetzige url zu setzen mit port 8080
-		//    evtl global config mit dem hostname?
-		importProvidersFrom(ApiModule.forRoot({rootUrl: window.location.protocol+"//"+window.location.hostname+""})),
+		importProvidersFrom(ApiModule.forRoot({rootUrl: environment.backendUrl})),
 		provideHttpClient(
 			withInterceptors([
 				(request: HttpRequest<any>, next: HttpHandlerFn) => {
